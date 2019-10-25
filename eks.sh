3464e68b2033325b037cc3a9d6a3b321833ea1a2
Teo Cheng Lim <chenglim.teo@thunes.com>
2019-10-25 12:47:31 +0800

    adding eks.sh helper

--- eks.sh
+++ eks.sh
@@ -4,10 +4,10 @@ endpoint=`aws eks describe-cluster --name $clusterName | grep endpoint\" | cut -
 IFS=$'\n'
 # create backup of /etc/hosts
 cp /etc/hosts /etc/hosts_backup
-sh -c "cat /etc/hosts  | grep -v $endpoint &gt; /etc/hosts_new"
+sh -c "cat /etc/hosts  | grep -v $endpoint > /etc/hosts_new"
 
 for item in $ips
 do
-  sh -c "echo $item  $endpoint &gt;&gt; /etc/hosts_new"
+  sh -c "echo $item  $endpoint >> /etc/hosts_new"
 done
-sh -c "cat /etc/hosts_new &gt; /etc/hosts"
+sh -c "cat /etc/hosts_new > /etc/hosts"
