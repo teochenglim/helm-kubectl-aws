a72311c9aa19667c61493362c73b9f762fb73cd9
Teo Cheng Lim <chenglim.teo@thunes.com>
2019-10-25 13:51:35 +0800

    adding eks.sh

--- eks.sh
+++ eks.sh
@@ -1,6 +1,7 @@
 clusterName=$1
-ips=`aws ec2  describe-network-interfaces --filters Name=description,Values="Amazon EKS $clusterName" | grep "PrivateIpAddress\"" | cut -d ":" -f 2 |  sed 's/[*",]//g' | sed 's/^\s*//'| uniq`
-endpoint=`aws eks describe-cluster --name $clusterName | grep endpoint\" | cut -d ":" -f 3 | sed 's/[\/,"]//g'`
+region=$2
+ips=`aws ec2 describe-network-interfaces --region $region --filters Name=description,Values="Amazon EKS $clusterName" | grep "PrivateIpAddress\"" | cut -d ":" -f 2 |  sed 's/[*",]//g' | sed 's/^\s*//'| uniq`
+endpoint=`aws eks describe-cluster --name $clusterName --region $region | grep endpoint\" | cut -d ":" -f 3 | sed 's/[\/,"]//g'`
 IFS=$'\n'
 # create backup of /etc/hosts
 cp /etc/hosts /etc/hosts_backup
